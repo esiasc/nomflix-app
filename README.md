fe2e975740fe7ad147731010cc59e55dc7cae28c
esiasc <esiascd@gmail.com>
2021-10-28 20:43:56 +0900

    2.2 React Router part Two

---
+++ README.md
@@ -0,0 +1,10 @@
+# Nomflix
+
+Learning React and ES6 y building a Movie Discovery App.
+
+## Screens
+
+- [ ] Home
+- [ ] TV Shows
+- [ ] Search
+- [ ] Detail
---
+++ src/Components/Router.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import Home from "Routes/Home";
+import TV from "Routes/TV";
+import Search from "Routes/Search";
+
+const route = () => (
+    <Router>
+        <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/tv" exact component={TV} />
+            <Route path="/search" exact component={Search} />
+            <Redirect from="*" to="/" />
+        </Switch>
+    </Router>
+);
+
+export default route
