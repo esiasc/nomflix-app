2f7af127319e2b6047edd59017bf8485106abb7c
esiasc <esiascd@gmail.com>
2021-11-02 14:23:30 +0900

    6.0 Presenter Structure

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,1 +1,15 @@
-export default () => "Home";
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "style-components";
+
+const HomePresenter = ({nowPlaying, popular, upcoming, loading, error}) => null;
+
+HomePresenter.propTypes = {
+    nowPlaying:PropTypes.array,
+    popular:PropTypes.array,
+    upcoming:PropTypes.array,
+    loading:PropTypes.bool.isRequired,
+    error:PropTypes.string
+};
+
+export default HomePresenter;
