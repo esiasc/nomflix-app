f0bc66602291697c3049ba7ab83ed1c43a74af49
esiasc <esiascd@gmail.com>
2021-11-04 20:55:21 +0900

    6.7 Poster Component part Two

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -51,7 +51,7 @@ const SearchPresenter = ({
                                 imageUrl={movie.poster_path} 
                                 title={movie.original_title} 
                                 rating={movie.vote_average} 
-                                year={movie.release_date.substring(0, 4)} 
+                                year={movie.release_date && movie.release_date.substring(0, 4)} 
                                 isMovie={true} 
                             />
                         ))}
@@ -66,7 +66,7 @@ const SearchPresenter = ({
                                 imageUrl={show.poster_path} 
                                 title={show.original_name} 
                                 rating={show.vote_average} 
-                                year={show.first_air_date.substring(0, 4)}
+                                year={show.first_air_date && show.first_air_date.substring(0, 4)}
                             />
                         ))}
                     </Section>
